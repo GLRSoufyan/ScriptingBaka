430dbb8b0d21d2ff9114590d019ee9afe1aa16f3
Soufyan B <101715@glr.nl>
2026-01-21 13:08:52 +0000

    oefening

---
+++ oefenen.php
@@ -0,0 +1,52 @@
+<?php
+
+$kleuren = [
+    "Groen" => "#00ff00",
+    "Rood" => "#ff0000",
+    "Blauw" => "#0000ff",
+    "Geel" => "#ffff00",
+    "Roze" => "#ff00ff",
+    "Cyaan" => "#00ffff",
+];
+
+$namen = [
+    "Xarg",
+    "Blip",
+    "Zorg",
+    "Mek",
+    "Tarn",
+    "Bloop",
+];
+
+class Alien{
+    public $Kleur;
+    public $KleurHex;
+    public $Naam;
+
+    public function __construct($Kleur = "Onbekend", $Naam = "Onbekend"){
+        $this->Kleur = $Kleur;
+        $this->Naam = $Naam;
+
+        if ($Kleur === "Onbekend") {
+            $this->Kleur = array_rand($GLOBALS['kleuren']);
+        }
+
+        if ($Naam === "Onbekend") {
+            $index = array_rand($GLOBALS['namen']);
+            $this->Naam = $GLOBALS['namen'][$index];
+        }
+
+        $this->KleurHex = $GLOBALS['kleuren'][$this->Kleur];
+        echo "<p style='color: {$this->KleurHex}'>
+        Een nieuwe alien is gemaakt: {$this->Naam} met kleur {$this->Kleur}
+      </p>";
+    }
+};
+
+$var = 0;
+while ($var <= 100) {
+    $var += 1;
+    $alien = new Alien();
+}
+
+?>
